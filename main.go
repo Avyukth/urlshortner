6f11b705652d633ccccde2014812115f79ae1b17
subha <subhrajit.makur@razorpay.com>
2019-07-12 21:04:06 +0530

    Database Connection Working

--- main.go
+++ main.go
@@ -1,24 +1,32 @@
 package main
 
 import (
+	"crypto/sha256"
 	"fmt"
 	"github.com/gin-gonic/gin"
-
 	//"database/sql"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/jinzhu/gorm"
 )
 var db *gorm.DB
+ var url="www.google.com"
 
 func main()  {
 	router:=gin.Default()
-	v1:=router.Group("/urlshort"){
-		v1.POST("/", createUrlShorter)
-	//	//v1.GET("/", fetchAllUrlShorter)
-	//	//v1.GET("/:id", fetchSingleUrlShorter)
-	//	//v1.PUT("/:id", updateUrlShorter)
+	//router.GET("/", init)
+
+	//router.GET("/" ,createUrlShorter)
+	v1:=router.Group("/urlshort")
+	{
+		v1.GET("/{id}", baseUrl)
+		v1.GET("/long", getLongUrl)
+		v1.PUT("/short", createUrlShorter)
+
+		//	//v1.GET("/", fetchAllUrlShorter)	//v1.PUT("/:id", updateUrlShorter)
 	//	//v1.DELETE("/:id", deleteUrlShorter)
 	}
+
+	router.Run()
 
 }
 func init()  {
@@ -32,17 +40,66 @@ func init()  {
 	db.AutoMigrate(&urlModel{})
 }
 
-func createUrlShorter(c *gin.Context)  {
+func getLongUrl(c *gin.Context)  {
 
 }
+func baseUrl(c *gin.Context)  {
+
+}
+func createUrlShorter(c *gin.Context)  {
+	fmt.Println("here")
+	////tmpl := template.Must(template.ParseFiles("forms.html"))
+	//
+	//message, _ := c.GetQuery("m")
+	//c.String(http.StatusOK, "Get works! you sent: "+message)
+	//c.HTML(http.StatusOK,"forms.html",gin.H{"title": "Page file title!!"})
+	//db,err=gorm.Open("mysql","root:root@/urlshort?charset=utf8&parseTime=True&loc=Local")
+	//if err != nil {
+	//	fmt.Println(err)
+	//	panic("failed to connect database " )
+	//}
+	urlH := sha256.New()
+	urlH.Write([]byte(url))
+	urlHashId:=urlH.Sum(nil)
+	shortenUrl:="www.bing.com"
+	shortenUrlH:= sha256.New()
+	shortenUrlH.Write([]byte(shortenUrl))
+	shortenUrlHId:=urlH.Sum(nil)
+	fmt.Println(shortenUrlHId)
+	data:=urlModel{UrlHashId:urlHashId,Url:url,Shorten:shortenUrl,ShortenHashId:shortenUrlHId}
+	fmt.Println(data)
+	//db.Exec("select * from url_models")
+	db.Debug().Create(&data)
+	//Also we can use save that will return primary key
+	//db.Debug().Save(&data)
+
+
+}
+
 type (
 	urlModel struct{
 		gorm.Model
-		UrlHashId string `gorm:"primary_key"`
+		UrlHashId []byte `gorm:"primary_key"`
 		Url string
 		Shorten string
-		ShortenHashId string `gorm:"primary_key"`
-
+		ShortenHashId []byte
 	}
 
 )
+
+var charToIndex map[string]int
+func idConverter()  {
+	initCharToIndex()
+	initIndexToChar()
+
+}
+func initCharToIndex()  {
+	for i := 0; i < 26; i++ {
+
+	}
+}
+func initIndexToChar()  {
+
+}
+
+b := [2]string{"Penn", "Teller"}
