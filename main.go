cbf9ada7a91412f093ad5137edba966615c8c39a
subha <subhrajit.makur@razorpay.com>
2019-07-15 16:49:07 +0530

    before data Retrieve

--- main.go
+++ main.go
@@ -6,73 +6,111 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/speps/go-hashids"
 	"net/http"
+	"strings"
 	"time"
-
 	//"database/sql"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/jinzhu/gorm"
 )
-var db *gorm.DB
- var url="www.google.com"
 
-func main()  {
-	router:=gin.Default()
+var db *gorm.DB
+var url = "www.google.com"
+
+//var bleveIdx bleve.Index
+//
+//// Bleve connect or create the index persistence
+//func Bleve(indexPath string) (bleve.Index, error) {
+//
+//	// with bleveIdx isn't set...
+//	if bleveIdx == nil {
+//		var err error
+//		// try to open de persistence file...
+//		bleveIdx, err = bleve.Open(indexPath)
+//		// if doesn't exists or something goes wrong...
+//		if err != nil {
+//			// create a new mapping file and create a new index
+//			mapping := bleve.NewIndexMapping()
+//			bleveIdx, err = bleve.New(indexPath, mapping)
+//			if err != nil {
+//				return nil, err
+//			}
+//		}
+//	}
+//
+//	// return de index
+//	return bleveIdx, nil
+//}
+
+func main() {
+	router := gin.Default()
 	//router.GET("/", init)
 
 	router.LoadHTMLGlob("views/*")
 	//router.GET("/" ,createUrlShorter)
-	v1:=router.Group("/")
+	v1 := router.Group("/")
 	{
 		v1.GET("/{id}", baseUrl)
-		v1.POST("/long", getLongUrl)
+		v1.GET("/get", getShortUrl)
+		v1.POST("/long", redirectShortUrl)
+		v1.POST("/created", cretaeShortUrl)
 		v1.GET("/short", createUrlShorter)
 
-	//	//	//v1.GET("/", fetchAllUrlShorter)	//v1.PUT("/:id", updateUrlShorter)
-	////	//v1.DELETE("/:id", deleteUrlShorter)
+		//	//	//v1.GET("/", fetchAllUrlShorter)	//v1.PUT("/:id", updateUrlShorter)
+		////	//v1.DELETE("/:id", deleteUrlShorter)
 	}
 
 	router.Run()
 
 }
-func init()  {
+func init() {
 	var err error
-	db,err=gorm.Open("mysql","root:root@/urlshort?charset=utf8&parseTime=True&loc=Local")
+	db, err = gorm.Open("mysql", "root:root@/urlshort?charset=utf8&parseTime=True&loc=Local")
 	if err != nil {
 		fmt.Println(err)
-		panic("failed to connect database " )
+		panic("failed to connect database ")
 	}
 	//Migrate the Schema
 	db.AutoMigrate(&urlModel{})
 }
 
-func getLongUrl(c *gin.Context)  {
-	fmt.Println("Hello ")
+func cretaeShortUrl(c *gin.Context) {
+	//fmt.Println("Hello ")
 	urlData := c.PostForm("url")
 	hd := hashids.NewData()
-	h,_ := hashids.NewWithData(hd)
+	h, _ := hashids.NewWithData(hd)
 	now := time.Now()
 	urlHashId, _ := h.Encode([]int{int(now.Unix())})
-	shortenUrl:= "http://rzp:3030/" + urlHashId
-
-
-	//urlH := sha256.New()
-	//urlH.Write([]byte(urlData))
-	//urlHashId:=urlH.Sum(nil)
-	//shortenUrl:="www.bing.com"
-	//shortenUrlH:= sha256.New()
-	//shortenUrlH.Write([]byte(shortenUrl))
-	//shortenUrlHId:=urlH.Sum(nil)
-	////fmt.Println(shortenUrlHId)
-	data:=urlModel{UrlHashId:urlHashId,Url:urlData,Shorten:shortenUrl}
+	shortenUrl := "http://rzp:3030/" + urlHashId
+	data := urlModel{UrlHashId: urlHashId, Url: urlData, Shorten: shortenUrl}
 	//fmt.Println(data)
 	//db.Exec("select * from url_models")
 	db.Debug().Create(&data)
+	c.HTML(http.StatusOK, "index.html", gin.H{
+		"shortU":shortenUrl,
+	})
 
 }
-func baseUrl(c *gin.Context)  {
+func getShortUrl(c *gin.Context) {
+
+	c.HTML(http.StatusOK, "shortForms.html", gin.H{
+		"title": "Users",
+	})
+}
+func redirectShortUrl(c *gin.Context) {
+	urlData := c.PostForm("url")
+	hashID:=strings.Split(urlData, "http://rzp:3030/")
+	out:=db.Where("url = ?", "url_hash_id").First(&hashID[1])
+	fmt.Println(out)
+	fmt.Println("hello again")
+
+
+
 
 }
-func createUrlShorter(c *gin.Context)  {
+func baseUrl(c *gin.Context) {
+
+}
+func createUrlShorter(c *gin.Context) {
 	fmt.Println("here")
 
 	////tmpl := template.Must(template.ParseFiles("forms.html"))
@@ -91,33 +129,31 @@ func createUrlShorter(c *gin.Context)  {
 		"title": "Users",
 	})
 
-
-
 }
 
 type (
-	urlModel struct{
+	urlModel struct {
 		gorm.Model
 		UrlHashId string `gorm:"primary_key"`
-		Url string
-		Shorten string
+		Url       string
+		Shorten   string
 		//ShortenHashId []byte
 	}
-
 )
 
 var charToIndex map[string]int
-func idConverter()  {
+
+func idConverter() {
 	initCharToIndex()
 	initIndexToChar()
 
 }
-func initCharToIndex()  {
+func initCharToIndex() {
 	for i := 0; i < 26; i++ {
 
 	}
 }
-func initIndexToChar()  {
+func initIndexToChar() {
 
 }
 
